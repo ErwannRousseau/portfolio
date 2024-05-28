29e8af9a8da29137639ab1d12238cb843e9fd835
ErwannRousseau <erwann.rousseau@icloud.com>
2024-05-28 18:08:06 +0200

    add nav and /blog pathname

--- components/ui/spacing.tsx
+++ components/ui/spacing.tsx
@@ -1,13 +1,14 @@
 import { cn } from "@/lib/utils";
 
 type SpacingProps = {
-  size?: "sm" | "md" | "lg";
+  size?: "xs" | "sm" | "md" | "lg";
 };
 
 export function Spacing({ size = "sm" }: SpacingProps) {
   return (
     <div
       className={cn({
+        "h-4": size === "xs",
         "h-8 lg:h-16": size === "sm",
         "h-16 lg:h-24": size === "md",
         "h-24 lg:h-32": size === "lg",
