1f849a5dd2fd37f9a452db7e9ae29bb54ba3be51
ErwannRousseau <erwann.rousseau@icloud.com>
2024-07-13 20:07:35 +0200

    feat: add a expand button in the code block

--- lib/highlighter.ts
+++ lib/highlighter.ts
@@ -2,46 +2,36 @@
 
 import { promises as fs } from "node:fs";
 import path from "node:path";
-import {
-  type BundledLanguage,
-  type BundledTheme,
-  type HighlighterGeneric,
-  createHighlighter,
-} from "shiki";
-
-let highlighterInstance: HighlighterGeneric<
-  BundledLanguage,
-  BundledTheme
-> | null = null;
+import { createHighlighter } from "shiki";
 
 export async function highlightCode(code: string, language: string) {
-  if (!highlighterInstance) {
-    const darkTheme = await fs.readFile(
-      path.join(process.cwd(), "lib/themes/dark.json"),
-      "utf-8",
-    );
+  const darkTheme = await fs.readFile(
+    path.join(process.cwd(), "lib/themes/dark.json"),
+    "utf-8",
+  );
 
-    const lightTheme = await fs.readFile(
-      path.join(process.cwd(), "lib/themes/light.json"),
-      "utf-8",
-    );
+  const lightTheme = await fs.readFile(
+    path.join(process.cwd(), "lib/themes/light.json"),
+    "utf-8",
+  );
 
-    highlighterInstance = await createHighlighter({
-      themes: [],
-      langs: [language],
-    });
+  const highlighter = await createHighlighter({
+    themes: [],
+    langs: [language],
+  });
 
-    await highlighterInstance.loadTheme(JSON.parse(darkTheme));
-    await highlighterInstance.loadTheme(JSON.parse(lightTheme));
-  }
+  await highlighter.loadTheme(JSON.parse(darkTheme));
+  await highlighter.loadTheme(JSON.parse(lightTheme));
 
-  const html = highlighterInstance.codeToHtml(code, {
+  const html = highlighter.codeToHtml(code, {
     lang: language,
     themes: {
       light: "Pace Light",
       dark: "Pace Dark",
     },
   });
+
+  highlighter.dispose();
 
   return html;
 }
