cc3be84dc4eb94d0ac4f55a135570735495b8504
ErwannRousseau <erwann.rousseau@icloud.com>
2024-07-14 20:46:05 +0200

    feat: improve code block by moving filename on top and moving loading.tsx at the root route

--- components/utils/code-block-wrapper.tsx
+++ components/utils/code-block-wrapper.tsx
@@ -20,30 +20,32 @@ export function CodeBlockWrapper({
   const [isOpened, setIsOpened] = React.useState(false);
 
   return (
-    <div
-      className="my-4 rounded-md border focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring"
-      {...props}
-    >
-      <div className="flex flex-col space-y-4">
-        <div className={cn("relative", !isOpened && "[&_pre]:max-h-[350px]")}>
-          {filename && (
-            <span className="absolute pt-1 pl-2 text-xs opacity-70">
-              {filename}
-            </span>
-          )}
-          <CopyButton value={code} className="absolute top-4 right-4" />
-          {children}
-          <div className="absolute inset-x-0 bottom-0 flex items-center justify-center rounded-b-md bg-gradient-to-b from-muted/30 to-primary/60 p-2">
-            <Button
-              variant="secondary"
-              className="h-8 text-xs"
-              onClick={() => setIsOpened(!isOpened)}
-            >
-              {isOpened ? t("collapse") : t("expand")}
-            </Button>
+    <>
+      {filename && (
+        <span className="mt-5 block pb-1 pl-2 text-xs opacity-80">
+          {filename}
+        </span>
+      )}
+      <div
+        className="relative mb-4 rounded-md border focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring"
+        {...props}
+      >
+        <div className="flex flex-col space-y-4">
+          <div className={cn("relative", !isOpened && "[&_pre]:max-h-[350px]")}>
+            <CopyButton value={code} className="absolute top-4 right-4" />
+            {children}
+            <div className="absolute inset-x-0 bottom-0 flex items-center justify-center rounded-b-md bg-gradient-to-b from-muted/30 to-primary/60 p-2">
+              <Button
+                variant="secondary"
+                className="h-8 text-xs"
+                onClick={() => setIsOpened(!isOpened)}
+              >
+                {isOpened ? t("collapse") : t("expand")}
+              </Button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
