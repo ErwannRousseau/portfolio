f99ffc598b9272e373afcee5a1dcd2f0a2e8732a
ErwannRousseau <erwann.rousseau@icloud.com>
2024-07-14 18:38:55 +0200

    remove next/dynamic

--- app/[lang]/blog/page.tsx
+++ app/[lang]/blog/page.tsx
@@ -1,23 +1,19 @@
 import BlogPage from "@/components/pages/blogpage";
 import type { Locale } from "@/i18n.config";
 import { loadBlogPage } from "@/sanity/lib/store";
-import { setStaticParamsLocale } from "next-international/server";
-import dynamic from "next/dynamic";
-import { draftMode } from "next/headers";
 
-const BlogPagePreview = dynamic(
-  () => import("@/sanity/preview/blogpage-preview"),
-);
+// const BlogPagePreview = dynamic(
+//   () => import("@/sanity/preview/blogpage-preview"),
+// );
 
 export default async function Blog({
   params: { lang },
 }: { params: { lang: Locale } }) {
-  setStaticParamsLocale(lang);
   const initial = await loadBlogPage(lang);
 
-  if (draftMode().isEnabled) {
-    return <BlogPagePreview initial={initial} lang={lang} />;
-  }
+  // if (draftMode().isEnabled) {
+  //   return <BlogPagePreview initial={initial} lang={lang} />;
+  // }
 
   return <BlogPage posts={initial.data} />;
 }
--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,22 +1,18 @@
 import HomePage from "@/components/pages/homepage";
 import type { Locale } from "@/i18n.config";
 import { loadHomePage } from "@/sanity/lib/store";
-import { setStaticParamsLocale } from "next-international/server";
-import dynamic from "next/dynamic";
-import { draftMode } from "next/headers";
 
-const HomePagePreview = dynamic(
-  () => import("@/sanity/preview/homepage-preview"),
-);
+// const HomePagePreview = dynamic(
+//   () => import("@/sanity/preview/homepage-preview"),
+// );
 
 export default async function Home({
   params: { lang },
 }: { params: { lang: Locale } }) {
-  setStaticParamsLocale(lang);
   const initial = await loadHomePage(lang);
-  if (draftMode().isEnabled) {
-    return <HomePagePreview initial={initial} lang={lang} />;
-  }
+  // if (draftMode().isEnabled) {
+  //   return <HomePagePreview initial={initial} lang={lang} />;
+  // }
 
   return <HomePage data={initial.data} />;
 }
