5477635899b0df04f5973404386dad6a0f658672
ErwannRousseau <erwann.rousseau@icloud.com>
2024-07-14 18:38:55 +0200

    remove next/dynamic

--- app/[lang]/blog/[slug]/page.tsx
+++ app/[lang]/blog/[slug]/page.tsx
@@ -1,13 +1,10 @@
 import PostPage from "@/components/pages/postpage";
 import type { Locale } from "@/i18n.config";
 import { loadPostPage } from "@/sanity/lib/store";
-import { setStaticParamsLocale } from "next-international/server";
-import dynamic from "next/dynamic";
-import { draftMode } from "next/headers";
 
-const PostPagePreview = dynamic(
-  () => import("@/sanity/preview/postpage-preview"),
-);
+// const PostPagePreview = dynamic(
+//   () => import("@/sanity/preview/postpage-preview"),
+// );
 
 export default async function Post(props: {
   params: { slug: string; lang: Locale };
@@ -15,12 +12,11 @@ export default async function Post(props: {
   const { params } = props;
   const { slug, lang } = params;
 
-  setStaticParamsLocale(lang);
   const initial = await loadPostPage(slug, lang);
 
-  if (draftMode().isEnabled) {
-    return <PostPagePreview initial={initial} params={params} />;
-  }
+  // if (draftMode().isEnabled) {
+  //   return <PostPagePreview initial={initial} params={params} />;
+  // }
 
   return <PostPage post={initial.data} />;
 }
