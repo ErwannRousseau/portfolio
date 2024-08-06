d8f38c2669298a965529279e489e38ac4e511e51
ErwannRousseau <erwann.rousseau@icloud.com>
2024-08-06 19:24:25 +0200

    update : light theme to default, remove unused file

--- components/home/connect.tsx
+++ components/home/connect.tsx
@@ -1,38 +1,49 @@
 "use server";
 
+import type { UrlObject } from "node:url";
 import { Section } from "@/components/ui/section";
 import { getI18n } from "@/lib/locales/server";
 import Link from "next/link";
 
 export default async function Connect() {
   const t = await getI18n();
+
+  const contacts = [
+    {
+      href: "mailto:erwann.rousseau@icloud.com?subject=Prise de contact",
+      label: "Email",
+    },
+    {
+      href: "https://www.linkedin.com/in/erwannrousseauwebdev/",
+      label: "LinkedIn",
+      target: "_blank",
+    },
+    {
+      href: "https://github.com/ErwannRousseau",
+      label: "GitHub",
+      target: "_blank",
+    },
+    {
+      href: "https://twitter.com/ErwannRousseau4",
+      label: "Twitter",
+      target: "_blank",
+    },
+  ];
+
   return (
     <Section className="flex-col">
       <h2>{t("Connect")}</h2>
       <ul className="contact-list flex w-fit gap-6 font-medium">
-        <li className="transition-opacit">
-          <Link href="mailto:erwann.rousseau@icloud.com?subject=Prise de contact">
-            Email
-          </Link>
-        </li>
-        <li className="transition-opacity">
-          <Link
-            href="https://www.linkedin.com/in/erwannrousseauwebdev/"
-            target="_blank"
-          >
-            LinkedIn
-          </Link>
-        </li>
-        <li className="transition-opacity">
-          <Link href="https://github.com/ErwannRousseau" target="_blank">
-            GitHub
-          </Link>
-        </li>
-        <li className="transition-opacity">
-          <Link href="https://twitter.com/ErwannRousseau4" target="_blank">
-            Twitter
-          </Link>
-        </li>
+        {contacts.map((contact) => (
+          <li key={contact.label} className="transition-opacity">
+            <Link
+              href={contact.href as unknown as UrlObject}
+              target={contact?.target}
+            >
+              {contact.label}
+            </Link>
+          </li>
+        ))}
       </ul>
     </Section>
   );
