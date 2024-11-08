5bd95e6b1033c3097b124b9d1d1fd35ddbf73dfc
ErwannRousseau <erwann.rousseau@icloud.com>
2024-11-08 17:48:32 +0100

    fix: change div to code for code block rendering and update styling in wrapper

--- components/utils/code-block.tsx
+++ components/utils/code-block.tsx
@@ -12,7 +12,7 @@ export default async function CodeBlock({
   const highlightedCode = await highlightCode(code, language);
 
   return (
-    <div
+    <code
       // biome-ignore lint/security/noDangerouslySetInnerHtml: This is a code block and we need to render the HTML.
       dangerouslySetInnerHTML={{ __html: highlightedCode }}
     />
