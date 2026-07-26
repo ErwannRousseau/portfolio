4c8174ec40aeb2f46ef18e7fe7b02c4948ab6730
ErwannRousseau <erwann.rousseau@icloud.com>
2026-07-26 17:17:15 -0400

    fix: restore blog post features

--- app/[lang]/blog/[slug]/page.tsx
+++ app/[lang]/blog/[slug]/page.tsx
@@ -1,17 +1,46 @@
+import { LikeButton } from "@/components/ui/like-button";
 import { Section } from "@/components/ui/section";
+import { Spacing } from "@/components/ui/spacing";
+import { Comments } from "@/components/utils/comments";
 import { CustomPortableText } from "@/components/utils/custom-portable-text";
 import { DateFormat } from "@/components/utils/date-format";
 import type { Locale } from "@/i18n.config";
-import { urlForImage } from "@/sanity/lib/image";
+import { getClientIp } from "@/lib/client-ip";
+import { urlForImage, urlForOpenGraphImage } from "@/sanity/lib/image";
 import { loadPostPage } from "@/sanity/lib/store";
+import type { Metadata } from "next";
 import type { PortableTextBlock } from "next-sanity";
 import Image from "next/image";
 
-export default async function Post(props: {
-  params: { slug: string; lang: Locale };
-}) {
-  const { params } = props;
-  const { slug, lang } = params;
+export async function generateMetadata({
+  params,
+}: Readonly<{
+  params: Promise<{ slug: string; lang: Locale }>;
+}>): Promise<Metadata> {
+  const { slug, lang } = await params;
+
+  const { data } = await loadPostPage(slug, lang);
+  const ogImage = urlForOpenGraphImage(data?.mainImage);
+
+  return {
+    title: data?.title,
+    description: data?.subtitle,
+    openGraph: {
+      title: data?.title,
+      description: data?.subtitle,
+      url: `https://erwannrousseau.dev/${lang}/blog/${slug}`,
+      images: ogImage,
+      type: "article",
+    },
+  };
+}
+
+export default async function Post({
+  params,
+}: Readonly<{
+  params: Promise<{ slug: string; lang: Locale }>;
+}>) {
+  const { slug, lang } = await params;
   const { data } = await loadPostPage(slug, lang);
 
   const imageUrl = (quality: number) =>
@@ -20,20 +49,30 @@ export default async function Post(props: {
   return (
     <main>
       <Section className="flex-col">
-        <article>
+        <article className="prose max-w-none">
           <Image
             alt={`post image ${data?.title}`}
             src={imageUrl(100) || ""}
-            className="mb-2 aspect-video rounded-md object-cover"
-            width={896}
-            height={304}
+            className="mb-2 aspect-video rounded-md"
+            width={768}
+            height={431}
             placeholder="blur"
             blurDataURL={imageUrl(50)}
           />
-          <DateFormat date={data?.publishedAt} />
-          <h1 className="py-4 text-center">{data?.title}</h1>
+          <div className="flex justify-between">
+            <DateFormat date={data?.publishedAt} />
+            <LikeButton
+              className="mr-2"
+              likes={data?.likeCount ?? 0}
+              liked={data?.likedBy?.includes(await getClientIp()) ?? false}
+              postId={data?._id}
+            />
+          </div>
+          <h1 className="pt-4 text-center">{data?.title}</h1>
           <CustomPortableText value={data?.body as PortableTextBlock[]} />
         </article>
+        <Spacing size="xs" />
+        <Comments lang={lang} />
       </Section>
     </main>
   );
