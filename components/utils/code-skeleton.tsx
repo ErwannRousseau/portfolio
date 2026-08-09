b1e39775053131f12d64d2fa53693146ae96f41a
ErwannRousseau <erwann.rousseau@icloud.com>
2026-08-09 17:14:16 +0200

    chore: remove useless skeleton component

--- components/utils/code-skeleton.tsx
+++ components/utils/code-skeleton.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Skeleton } from "@/components/ui/skeleton";
 import { MAX_NUMBER_OF_LINES } from "@/lib/constants";
 import { cn } from "@/lib/utils";
 
@@ -27,7 +26,7 @@ export function CodeSkeleton({ numberOfLines }: { numberOfLines: number }) {
       className={cn("flex flex-col gap-3 p-6", numberOfLines === 1 && "p-3")}
     >
       {skeletons.map(({ key, width }) => (
-        <Skeleton key={key} className={"h-4"} style={{ width }} />
+        <div key={key} className="h-4 animate-pulse rounded-md bg-primary/10" style={{ width }} />
       ))}
     </div>
   );
